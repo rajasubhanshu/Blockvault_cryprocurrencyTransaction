290a50d34d2b53803f4e6632cf3b5977260e3c83
rajasubhanshu <99963364+rajasubhanshu@users.noreply.github.com>
2023-05-21 23:48:47 +0530

    Update WalletCard.jsx

--- src/component/WalletCard.jsx
+++ src/component/WalletCard.jsx
@@ -1,62 +1,76 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { AiFillPlayCircle } from "react-icons/ai";
-const provider = new ethers.providers.Web3Provider(window.ethereum);
+
 const WalletCard = () => {
     const [errorMessage, setErrorMessage] = useState(null);
     const [defaultAccount, setDefaultAccount] = useState(null);
     const [userBalance, setUserBalance] = useState(null);
-    const connectwalletHandler = () => {
-        if (window.ethereum) {
-            provider.send("eth_requestAccounts", []).then(async () => {
-                await accountChangedHandler(provider.getSigner());
-            })
-        } else {
-            setErrorMessage("Please Install Metamask!!!");
+
+    useEffect(() => {
+        const checkMetaMask = async () => {
+            if (window.ethereum) {
+                try {
+                    await window.ethereum.request({ method: 'eth_requestAccounts' });
+                    const provider = new ethers.providers.Web3Provider(window.ethereum);
+                    await accountChangedHandler(provider.getSigner());
+                } catch (err) {
+                    setErrorMessage(err.message);
+                }
+            } else {
+                setErrorMessage("Please Install Metamask!!!");
+            }
+        };
+
+        checkMetaMask();
+    }, []);
+
+    const connectWalletHandler = async () => {
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            await accountChangedHandler(provider.getSigner());
+        } catch (err) {
+            setErrorMessage(err.message);
         }
-    }
+    };
+
     const accountChangedHandler = async (newAccount) => {
-        const address = await newAccount.getAddress();
-        setDefaultAccount(address);
-        const balance = await newAccount.getBalance()
-        setUserBalance(ethers.utils.formatEther(balance));
-        await getuserBalance(address)
-    }
-    const getuserBalance = async (address) => {
-                const balance = await provider.getBalance(address, "latest")
-        setUserBalance(ethers.utils.formatEther(balance));
-    }
+        try {
+            const address = await newAccount.getAddress();
+            setDefaultAccount(address);
+            const balance = await newAccount.getBalance();
+            setUserBalance(ethers.utils.formatEther(balance));
+        } catch (err) {
+            setErrorMessage(err.message);
+        }
+    };
+
     return (
-        <div className="WalletCard   ">
+        <div className="WalletCard">
             <button
+            
                 type="button"
-                className="flex flex-row justify-center items-center bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2952e3]"
-                onClick={connectwalletHandler}
-                style={{ background: defaultAccount ? "rgba(255, 13, 13, 0.63) " : "rgba(242, 244, 12, 1) " }}
+                className={`flex flex-row justify-center items-center bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2952e3] ${defaultAccount ? 'bg-red-400' : 'bg-yellow-400'}`}
+                onClick={connectWalletHandler}
             >
                 <AiFillPlayCircle className="text-black mr-2" />
-                <p className="text-Black text-base font-semibold">
+                <p className="text-black text-base font-semibold">
                     {defaultAccount ? "Connected!!" : "Connect Wallet"}
                 </p>
             </button>
-            {/* <button
-                style={{ background: defaultAccount ? "#A5CC82" : "white" }}
-                onClick={connectwalletHandler}>
-                {defaultAccount ? "Connected!!" : "Connect Wallet"}
-            </button> */}
-            <div className="displayAccount  mx-4 my-2">
-                <h4 className="walletAddress text-white text-xl "> Address:{defaultAccount}</h4>
+
+            <div className="displayAccount mx-4 my-2">
+                <h4 className="walletAddress text-white text-xl">Address: {defaultAccount}</h4>
                 <div className="balanceDisplay text-white text-xl my-auto">
                     <h3>
                         Wallet Amount: {userBalance}
                     </h3>
                 </div>
             </div>
-            {errorMessage}
+            {errorMessage && <div className="text-blue">{errorMessage}</div>}
         </div>
-        
-    )
-    
-}
+    );
+};
+
 export default WalletCard;
